e2773128fbe057fe523d5a065a7687c720deca1f
Kevin Murning <kevinmurning@gmailc.om>
2021-12-20 13:36:36 -0500

    preprocess clean up

--- lda/preprocess_tweets.py
+++ lda/preprocess_tweets.py
@@ -4,95 +4,70 @@ from nltk.corpus import stopwords, wordnet
 import nltk
 from nltk.stem import WordNetLemmatizer
 
-punctuation = '!"$%&\'()*+,-./:;<=>?[\\]^_`{|}~•@'  # define a string of punctuation symbols
+punctuation = '!"$%&\'()*+,-./:;<=>?[\\]^_`{|}~•@'
 
-
-# Functions to clean tweets
-def remove_links(tweet):
-    """Takes a string and removes web links from it"""
-    tweet = re.sub(r'http\S+', '', tweet)  # remove http links
-    tweet = re.sub(r'bit.ly/\S+', '', tweet)  # remove bitly links
-    tweet = tweet.strip('[link]')  # remove [links]
-    tweet = re.sub(r'pic.twitter\S+', '', tweet)
-    return tweet
-
-
-def remove_users(tweet):
-    """Takes a string and removes retweet and @user information"""
-    tweet = re.sub('(RT\s@[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet)  # remove re-tweet
-    tweet = re.sub('(@[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet)  # remove tweeted at
-    return tweet
-
-
-def remove_hashtags(tweet):
-    """Takes a string and removes any hash tags"""
-    tweet = re.sub('(#[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet)  # remove hash tags
-    return tweet
-
-
-def remove_av(tweet):
-    """Takes a string and removes AUDIO/VIDEO tags or labels"""
-    tweet = re.sub('VIDEO:', '', tweet)  # remove 'VIDEO:' from start of tweet
-    tweet = re.sub('AUDIO:', '', tweet)  # remove 'AUDIO:' from start of tweet
-    return tweet
-
-
-def tokenize(tweet):
-    """Returns tokenized representation of words in lemma form excluding stopwords"""
+def tokenize_and_lemmatize(tweet):
     result = []
     for token in gensim.utils.simple_preprocess(tweet):
-        if token not in gensim.parsing.preprocessing.STOPWORDS \
-                and len(token) > 2:  # drops words with less than 3 characters
-            result.append(lemmatize(token))
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 2:
+            result.append(WordNetLemmatizer().lemmatize(token, pos='v'))
     return result
 
 
-def lemmatize(token):
-    """Returns lemmatization of a token"""
-    return WordNetLemmatizer().lemmatize(token, pos='v')
-
-
-def remove_unrecognized_words(my_list_of_strings):
+def remove_unrecognized_words(tokenized_tweets):
     wpt = nltk.WordPunctTokenizer()
-    only_recognized_words = []
+    recognized_words = []
 
-    for s in my_list_of_strings:
-        tokens = wpt.tokenize(s)
-        if tokens:  # check if empty string
-            for t in tokens:
-                if wordnet.synsets(t):
-                    only_recognized_words.append(t)  # only keep recognized words
+    for tweet in tokenized_tweets:
+        tokens = wpt.tokenize(tweet)
+        if tokens:
+            for token in tokens:
+                if wordnet.synsets(token):
+                    recognized_words.append(token)
 
-    return only_recognized_words
+    return recognized_words
 
 
 def preprocess_tweet(tweet):
-    """Main master function to clean tweets, stripping noisy characters, and tokenizing use lemmatization"""
-    tweet = remove_users(tweet)
-    tweet = remove_links(tweet)
-    tweet = remove_hashtags(tweet)
 
-    tweet = remove_av(tweet)
+    # Remove Retweets
+    tweet = re.sub('(RT\s@[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet)
+    #Remove Mentions
+    tweet = re.sub('(@[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet)
 
-    tweet = tweet.lower()  # lower case
-    tweet = re.sub('[' + punctuation + ']+', ' ', tweet)  # strip punctuation
-    tweet = re.sub('\s+', ' ', tweet)  # remove double spacing
-    tweet = re.sub('([0-9]+)', '', tweet)  # remove numbers
-    tweet_token_list = tokenize(tweet)  # apply lemmatization and tokenization
+    # Remove http links
+    tweet = re.sub(r'http\S+', '', tweet)
+    # Remove Bitly links
+    tweet = re.sub(r'bit.ly/\S+', '', tweet)
+    # remove other links
+    tweet = tweet.strip('[link]')
+    # remove picture links
+    tweet = re.sub(r'pic.twitter\S+', '', tweet)
+
+    # remove hashtags
+    tweet = re.sub('(#[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet)
+
+    # Remove video
+    tweet = re.sub('VIDEO:', '', tweet)
+    # Remove audio
+    tweet = re.sub('AUDIO:', '', tweet)
+
+    # lower case
+    tweet = tweet.lower()
+
+    # Remove punctuation
+    tweet = re.sub('[' + punctuation + ']+', ' ', tweet)
+    # Remove double spacing
+    tweet = re.sub('\s+', ' ', tweet)
+    # Remove numbers
+    tweet = re.sub('([0-9]+)', '', tweet)
+    # Lemmatization and tokenization
+    tweet_token_list = tokenize_and_lemmatize(tweet)
+    # Remove unrecognized words by wordnet
     tweet = remove_unrecognized_words(tweet_token_list)
-    #     tweet = tweet_token_list
     return tweet
 
 
 def tokenize_tweets(df):
-    """Main function to read in and return cleaned and preprocessed dataframe.
-    This can be used in Jupyter notebooks by importing this module and calling the tokenize_tweets() function
-    Args:
-        df = data frame object to apply cleaning to
-    Returns:
-        pandas data frame with cleaned tokens
-    """
-
     df['tokens'] = df.text.apply(preprocess_tweet)
-    num_tweets = len(df)
     return df
